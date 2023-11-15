5ace3ab531e4c5ad2f4af168eb1d8ba191466d7c
SilverSoul <z1079931505@gmail.com>
2023-11-15 18:28:04 -0500

    add last update request js

---
+++ assets/js/last_updated_date.js
@@ -0,0 +1,27 @@
+const desiredRepo = "silvery107.github.io";
+const dateTagClass = ".date";
+const months = ["Jan.", "Feb.", "Mar.", "Apr.", "May", "Jun.", "Jul.", "Aug.", "Sep.", "Oct.", "Nov.", "Dec."];
+
+var xhttp = new XMLHttpRequest();
+xhttp.onreadystatechange = function()
+{
+  if (this.readyState == 4 && this.status == 200)
+  {
+    let repo = JSON.parse(this.responseText);
+
+    var lastUpdated = new Date(repo.pushed_at);
+    var day = lastUpdated.getUTCDate();
+    var month = lastUpdated.getUTCMonth();
+    var year = lastUpdated.getUTCFullYear();
+    $(dateTagClass).text(`Updated ${months[month]} ${day}, ${year}`);
+  }
+  else if (this.readyState == 4 && this.status == 401) 
+  {
+    $(dateTagClass).text(`Token Expired!`);
+  }
+};
+
+xhttp.open("GET", `https://api.github.com/repos/silvery107/${desiredRepo}`, true);
+xhttp.setRequestHeader('Authorization', 'Bearer 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');
+
+xhttp.send();
